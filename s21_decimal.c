67be85daa3b4f559137835be3556865754bd5894
Gryffindor_Dodie <sultanovartur1873@gmail.com>
2022-06-21 21:56:19 +0300

    s21_add processing, its so difficult without s21_sub

--- s21_decimal.c
+++ s21_decimal.c
@@ -9,20 +9,76 @@
  * @return s21_decimal число с выставленным value_type
  */
 
-s21_decimal res_for_add(s21_decimal num1, s21_decimal num2) {
-    s21_decimal result = {{0, 0, 0, 0}, s21_NORMAL_VALUE};
-    /*
-    num1      num2
-    +inf      +-normal  = +inf    - в простой проверке
-    -inf      +-normal  = -inf    - в простой проверке
-    nan       any       = nan     - в простой проверке
-    +inf      -inf      = nan     - отдельная
-    */
+
+
+s21_decimal bit_add(s21_decimal *a, s21_decimal *b, int error_code) {
+    s21_decimal result = {0 ,0, 0, 0};
+    size_t buffer = 0;
+    for (int i = 0; i < 96; i++) {
+        int current_bit_a = get_bit(*a, i);
+        int current_bit_b = get_bit(*b, i);
+
+        if (!current_bit_a && !current_bit_b) { // оба бита выключены
+            if (buffer) { // если в буфере что-то есть значит мы повышаем порядок, т.е. прошлый бит 0 , а этот будет 1
+                set_bit(&result, i, 1);
+                buffer = 0;
+            } else {
+                set_bit(&result, i, 0);
+            }
+        } else if (current_bit_a != current_bit_b) { // Один включен
+            if (buffer) {
+                set_bit(&result, i, 0);
+                buffer = 1;
+            } else {
+                set_bit(&result, i, 1);
+            }
+        } else { // Оба вкл
+            if (buffer) {
+                set_bit(&result, i, 1);
+            } else {
+                set_bit(&result, i, 0);
+                buffer = 1;
+            }
+        }
+
+        if (i == 95 && buffer == 1) {
+            error_code = 1;
+        }
+    }
 
     return result;
 }
 
+int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
+    int solution = 0;
+    // написать чек на допустимость значения (хз надо ли)
+    // пока обрабатывает только целые числа(scale на подходе)
+    if (!get_sign(&value_1) && !get_sign(&value_2)) {
 
+        if (get_scale(&value_1) != get_scale(&value_2)) {
+            // scale...
+        }
+        int err;
+        *result = bit_add(&value_1, &value_2, err);
+        if (err == 1) {
+            // infinity
+            solution = 1;
+        }
+    } else if (get_sign(&value_1) && !get_sign(&value_2)) {
+        // нужно вычитание
+    } else if (!get_sign(&value_1) && get_sign(&value_2)) {
+        // нужно вычитание
+    } else {
+        // оба отрицательных
+        set_sign(&value_1, 0);
+        set_sign(&value_2, 0);
+        // нужно сложить каким-то образом(bit_add)
+        set_sign(&result, 1);
+    }
+
+
+    return solution;
+}
 
 int get_bit(const s21_decimal a, int bit_number) {
     int result = 0;
