54714a1aa4baaa6aa19e8fa93eba6e5be54eec7c
Gryffindor_Dodie <sultanovartur1873@gmail.com>
2022-07-02 14:08:34 +0300

    add and fix all cmparisons functions

--- s21_decimal.c
+++ s21_decimal.c
@@ -104,8 +104,6 @@ int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //                                   СРАВНЕНИЯ                                                            //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
 /**
  *
  * @param value_1
@@ -181,6 +179,24 @@ int s21_is_equal(s21_decimal value_1, s21_decimal value_2) {
 }
 
 
+/**
+ *
+ * @param value_1
+ * @param value_2
+ * @return 1 - true, 0 - false
+ */
+int s21_is_not_equal(s21_decimal value_1, s21_decimal value_2) {
+    return (!s21_is_equal(value_1, value_2));
+}
+
+
+int s21_is_greater_or_equal(s21_decimal value_1, s21_decimal value_2) {
+    return !!(s21_is_greater(value_1, value_2) || s21_is_equal(value_1, value_2));
+}
+
+int s21_is_less_or_equal(s21_decimal value_1, s21_decimal value_2) {
+    return !!(s21_is_less(value_1, value_2) || s21_is_equal(value_1, value_2));
+}
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
--- s21_decimal.h
+++ s21_decimal.h
@@ -56,9 +56,14 @@ int s21_from_decimal_to_int(s21_decimal src, int *dst);
 
 int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
 
+
+
 int s21_is_greater(s21_decimal value_1, s21_decimal value_2);
 int s21_is_less(s21_decimal dec1, s21_decimal dec2);
 int s21_is_equal(s21_decimal value_1, s21_decimal value_2);
+int s21_is_not_equal(s21_decimal value_1, s21_decimal value_2);
+int s21_is_greater_or_equal(s21_decimal value_1, s21_decimal value_2);
+int s21_is_less_or_equal(s21_decimal value_1, s21_decimal value_2);
 
 #endif // _S21_DECIMAL_H_
 //#endif // SRC_S21_DECIMAL_H_
